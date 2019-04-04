7e5ac28cca137894d5d1e25e6854701fe5fe99d7
Werevolk <werevolk@gmail.com>
2019-04-04 05:33:56 +0000

    add buckets

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-north1"
+}
+
+variable zone {
+  description = "Zone"
+
+  # Значение по умолчанию
+  default = "europe-north1-b"
+}
